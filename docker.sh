ae967c963e1729faacd2bd26aea7da9cadc96888
MetaBooy <116703007+MetaBooy@users.noreply.github.com>
2025-09-27 05:39:07 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 sudo apt update && sudo apt install -y dos2unix wget && \
-wget https://raw.githubusercontent.com/Avinashtok/docker-install/main/install_docker_full.sh && \
+wget https://raw.githubusercontent.com/MetaBooy/gensynnode/refs/heads/main/dockerf.sh && \
 dos2unix install_docker_full.sh && chmod +x install_docker_full.sh && sudo ./install_docker_full.sh && \
 sudo systemctl enable --now docker && sudo usermod -aG docker $USER && newgrp docker && docker run hello-world
