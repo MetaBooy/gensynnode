e0374a9be91229a012f2a639dff1b5391b1434c9
MetaBooy <116703007+MetaBooy@users.noreply.github.com>
2025-09-27 05:55:15 +0530

    Update and rename docker.sh to 0

--- docker.sh
+++
@@ -1,4 +0,0 @@
-sudo apt update && sudo apt install -y dos2unix wget && \
-wget https://raw.githubusercontent.com/MetaBooy/gensynnode/refs/heads/main/dockerf.sh && \
-dos2unix install_docker_full.sh && chmod +x install_docker_full.sh && sudo ./install_docker_full.sh && \
-sudo systemctl enable --now docker && sudo usermod -aG docker $USER && newgrp docker && docker run hello-world
